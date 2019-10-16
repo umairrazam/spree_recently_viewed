19da532d25e261d78e400b93605d9c591fa5f07e
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-16 12:08:41 +0200

    Adds deface to pree_recently_viewed.rb

--- lib/spree_recently_viewed.rb
+++ lib/spree_recently_viewed.rb
@@ -3,6 +3,7 @@ require 'spree_recently_viewed/engine'
 require 'spree_recently_viewed/version'
 require 'coffee_script'
 require 'sass/rails'
+require 'deface'
 
 module Spree
   module RecentlyViewed
