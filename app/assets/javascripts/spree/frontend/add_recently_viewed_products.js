a19f3c95e23f25ffec7ba71bcdf502c006b1f09a
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-16 12:01:50 +0200

    Replaces coffee script with plain js

--- app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
+++ app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
@@ -1,5 +1,7 @@
-Spree.fetch_recently_viewed_products = (productId = "")->
-  $.ajax
+Spree.fetch_recently_viewed_products = (productId = "") => {
+  $.ajax({
     url: Spree.pathFor("recently_viewed_products?product_id=" + productId),
-    success: (data) ->
+    success: (data) =>
       $("#recently_viewed_container").html(data)
+  })
+};
