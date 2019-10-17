bf231036ea829e4a68ee55d5e305fbf466ea6980
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-17 09:12:27 +0200

    Removes prepend from base_helper_decorator.rb

--- app/controllers/spree/helpers/spree/base_helper_decorator.rb
+++ app/controllers/spree/helpers/spree/base_helper_decorator.rb
@@ -1,12 +1,10 @@
 module Spree::BaseHelperDecorator
-  def self.prepended(base)
-    def base.cached_recently_viewed_products_ids
-      (cookies['recently_viewed_products'] || '').split(', ')
-    end
+  def cached_recently_viewed_products_ids
+    (cookies['recently_viewed_products'] || '').split(', ')
+  end
 
-    def base.cached_recently_viewed_products
-      Spree::Product.new.find_by_array_of_ids(cached_recently_viewed_products_ids)
-    end
+  def cached_recently_viewed_products
+    Spree::Product.new.find_by_array_of_ids(cached_recently_viewed_products_ids)
   end
 end
 
