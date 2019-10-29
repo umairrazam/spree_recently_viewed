c49f9fd9282d38379b8c42a0570afb11c7c94ae4
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-29 08:10:56 +0100

    Moves helper to app/helpers

--- app/helpers/spree/recently_viewed_products_helper.rb
+++ app/helpers/spree/recently_viewed_products_helper.rb
@@ -1,9 +1,9 @@
 module Spree
-  module ProductsHelper
+  module RecentlyViewedProductsHelper
     def cached_recently_viewed_products_ids
       (cookies['recently_viewed_products'] || '').split(', ')
     end
-  
+
     def cached_recently_viewed_products
       Spree::Product.find_by_array_of_ids(cached_recently_viewed_products_ids)
     end
