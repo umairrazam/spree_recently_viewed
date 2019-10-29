6074866f83685f3b5f2fd9cb2265f94512a8fffd
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-29 08:17:03 +0100

    Corrects add_recently_viewed_products override

--- app/overrides/spree/products/show/add_recently_viewed_products.rb
+++ app/overrides/spree/products/show/add_recently_viewed_products.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'add_recently_viewed_products_to_products_show',
-  insert_after: "#product_description, [data-hook='product_description']",
+  insert_after: "#product-description, [data-hook='product_description']",
   partial: 'spree/shared/add_recently_viewed_products'
 )
